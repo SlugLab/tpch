c9413501aad7e435081a12302f9ff4a4d4fb23d7
victoryang00 <yangyiwei2000@gmail.com>
2022-08-15 19:18:14 -0700

    add calculated result

--- monetdb-tpch/03_run/parse_perf_result.py
+++ monetdb-tpch/03_run/parse_perf_result.py
@@ -1,10 +1,9 @@
 import os
-import re
 
 load_lookup=[]
 load_miss=[]
-load_modified=[]
-load_sstate=[]
+load_hitm=[]
+load_remote_hitm=[]
 
 for item in os.listdir("./"):
     if str(item).endswith("txt"):
@@ -13,16 +12,31 @@ for item in os.listdir("./"):
         for line in lines:
             if line.startswith("  Load LLC hit                      :"):
                 load_lookup.append(int(line[40:].strip()))
-            if line.startswith("  Load Local HITM                   : "):
-                load_modified.append(int(line[40:].strip()))
-            if line.startswith("  Load MESI State Shared            :"):
-                load_sstate.append(int(line[40:].strip()))
+            if line.startswith("  Load Local HITM                   :"):
+                load_hitm.append(int(line[40:].strip()))
+            if line.startswith("  Load Remote HITM                  :"):
+                load_remote_hitm.append(int(line[40:].strip()))
             if line.startswith("  Load LLC Misses                   :"):
                 load_miss.append((int(line[40:].strip())))
 
 print(len(load_lookup))
 print(len(load_miss))
+count_file=-1
 for item in os.listdir("./"):
-    if str(item) .endswith("0.csv") or str(item) .endswith("1.csv"):
+    if str(item).endswith("0.csv") or str(item).endswith("1.csv"):
+        count_file+=1
+        f = open(item,'r')
+        res = "dbname,seqno,query,exec_time,perf_dev,dev_pcnt,perf_stts,rss,avgrss,vsz,memory,llc_hit,llc_hitm,llc_miss\n"
+        lines = f.readlines()
+        count=-1
+        for line in lines:
+            count+=1
+            if 0<count<=21:
+                index =(count_file*21+count)%84
+                print(count_file,count)
+                res+=line.strip()+","+str(load_lookup[index])+","+str(load_hitm[index]+load_remote_hitm[index])+","+str(load_miss[index])+"\n"
+        print(res)
         f = open(item,'w')
+
+        f.write(res)
         
